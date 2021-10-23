36661f8c48c0cc8b5f5abc2bc31d271700dad28d
rrrshimin <rrr.shimin@gmail.com>
2021-10-23 22:34:37 +0300

    Initial Commit

---
+++ Feedmi/FeedmiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedmiApp.swift
+//  Feedmi
+//
+//  Created by Roman Shimin on 10/23/21.
+//
+
+import SwiftUI
+
+@main
+struct FeedmiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
